de2718f8778dfa806b5636eea0c3d6abf311ee59
kidonchu <kidon@sterilngtrader.com>
2013-02-13 13:36:06 -0600

    Modify sql query to create logs table

--- sql/vsdl.sql
+++ sql/vsdl.sql
@@ -1,6 +1,7 @@
 CREATE TABLE logs (
-    id 		BIGINT      NOT NULL auto_increment,
-    time	TIMESTAMP   NOT NULL,
-    log 	TEXT        NOT NULL,
-    PRIMARY KEY (id)
+   	id 		BIGINT      	NOT NULL auto_increment,
+    	time		TIMESTAMP   	NOT NULL,
+    	log 		TEXT        	NOT NULL,
+	category	VARCHAR(255)	NOT NULL,
+	PRIMARY KEY (id)
 );
